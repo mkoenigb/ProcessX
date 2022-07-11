bb6433e73d840b024f096dba02558b24d69ab836
mkoenigb <70903958+mkoenigb@users.noreply.github.com>
2022-07-11 18:05:45 +0200

    Add files via upload

--- algorithms/vector_creation/CreateNestedGrid.py
+++ algorithms/vector_creation/CreateNestedGrid.py
@@ -18,7 +18,8 @@ import operator, processing, math
 from PyQt5.QtCore import QCoreApplication, QVariant
 from qgis.core import (QgsField, QgsFields, QgsFeature, QgsProcessing, QgsExpression, QgsSpatialIndex, QgsGeometryEngine, QgsGeometry, QgsPointXY, QgsPoint, QgsRectangle, QgsWkbTypes, 
                        QgsFeatureSink, QgsFeatureRequest, QgsProcessingAlgorithm, QgsExpressionContext, QgsExpressionContextUtils,
-                       QgsProcessingParameterFeatureSink, QgsProcessingParameterField, QgsProcessingParameterExtent, QgsProcessingParameterDistance, QgsProcessingParameterFeatureSource, QgsProcessingParameterEnum, QgsProcessingParameterExpression, QgsProcessingParameterNumber, QgsProcessingParameterString)
+                       QgsProcessingParameterFeatureSink, QgsProcessingParameterBoolean, QgsProcessingParameterField, QgsProcessingParameterExtent, QgsProcessingParameterDistance, QgsProcessingParameterFeatureSource, 
+                       QgsProcessingParameterEnum, QgsProcessingParameterExpression, QgsProcessingParameterNumber, QgsProcessingParameterString)
 
 class CreateNestedGrid(QgsProcessingAlgorithm):
     EXTENT = 'EXTENT'
@@ -27,6 +28,9 @@ class CreateNestedGrid(QgsProcessingAlgorithm):
     YSPACING = 'YSPACING'
     SUBGRIDS = 'SUBGRIDS'
     LETTERS = 'LETTERS'
+    XFACTOR = 'XFACTOR'
+    YFACTOR = 'YFACTOR'
+    STARTWITHPARENT = 'STARTWITHPARENT'
     OUTPUT = 'OUTPUT'
     
     # Source: https://stackoverflow.com/a/12334507 (modified)
@@ -66,19 +70,29 @@ class CreateNestedGrid(QgsProcessingAlgorithm):
             QgsProcessingParameterExtent(
                 self.EXTENT, self.tr('Extent')))
         self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.STARTWITHPARENT, self.tr('Start with parentgrid, so larger grids lie behind their childs (if unchecked, the largest grid lies on top and childs are not initially visible)'), defaultValue = 0))
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.SUBGRIDS, self.tr('Number of Subgrids incl. Parentgrid (1 means only Parent-Grid)'), minValue = 1, maxValue = 999, defaultValue = 3, type = 0))
+        self.addParameter(
             QgsProcessingParameterNumber(
                 self.XSPACING, self.tr('X-Spacing of Parent-Grid in Extent-CRS-Units'), minValue = 0.000001, defaultValue = 1000, type = 1))
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.XFACTOR, self.tr('X-Factor to divide X-Spacing by for each childgrid'), minValue = 2, maxValue = 9999, defaultValue = 3, type = 0))
         self.addParameter(
             QgsProcessingParameterNumber(
                 self.YSPACING, self.tr('Y-Spacing of Parent-Grid in Extent-CRS-Units'), minValue = 0.000001, defaultValue = 1000, type = 1))
         self.addParameter(
             QgsProcessingParameterNumber(
-                self.SUBGRIDS, self.tr('Number of Subgrids incl. Parentgrid (1 means only Parent-Grid)'), minValue = 1, maxValue = 999, defaultValue = 3, type = 0))
+                self.YFACTOR, self.tr('Y-Factor to divide Y-Spacing by for each childgrid'), minValue = 2, maxValue = 9999, defaultValue = 4, type = 0))
         self.addParameter(
             QgsProcessingParameterFeatureSink(
                 self.OUTPUT, self.tr('Grid')))
 
     def processAlgorithm(self, parameters, context, feedback):
+        feedback.setProgressText('Prepare processing...')
         gridtype = self.parameterAsInt(parameters, self.GRIDTYPE, context)
         extent_rect = self.parameterAsExtent(parameters, self.EXTENT, context)
         extent_crs = self.parameterAsExtentCrs(parameters, self.EXTENT, context)
@@ -86,13 +100,15 @@ class CreateNestedGrid(QgsProcessingAlgorithm):
         xspacing = self.parameterAsDouble(parameters, self.XSPACING, context)
         yspacing = self.parameterAsDouble(parameters, self.YSPACING, context)
         subgrids = self.parameterAsInt(parameters, self.SUBGRIDS, context)
+        xfactor = self.parameterAsDouble(parameters, self.XFACTOR, context)
+        yfactor = self.parameterAsDouble(parameters, self.YFACTOR, context)
+        startwithparent = self.parameterAsBool(parameters, self.STARTWITHPARENT, context)
         polygonsides = {
             0: 4, # Rectangle
             1: 6 # Hexagon
         }
         letters = self.parameterAsEnums(parameters, self.LETTERS, context)
         polygonsides = polygonsides[gridtype]
-        feedback.setProgressText('Prepare processing...')
         
         if not extent_rect.isFinite():
             feedback.reportError('The chosen extent is not finite!', fatalError = True)
@@ -125,36 +141,45 @@ class CreateNestedGrid(QgsProcessingAlgorithm):
         
         (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT, context, output_layer_fields, QgsWkbTypes.Polygon, extent_crs)
         
-        n_parentgrids_x = int(math.ceil(extent_rect.width() / xspacing))
-        n_parentgrids_y = int(math.ceil(extent_rect.height() / yspacing))
-        n_parentgrids_t = (n_parentgrids_x * n_parentgrids_y)
-        n_totalcells = 0
-        cells_per_subgrid = {}
-        for i in range(1,subgrids+1):
-            childgrids_per_parentgrid = ((2**(i-1))**2)
-            n_totalcells += childgrids_per_parentgrid * n_parentgrids_t
-            cells_per_subgrid[i] = childgrids_per_parentgrid * n_parentgrids_t
-        # actually use extent from parent grid:
-        extent_rect_new = QgsRectangle(extent_rect.xMinimum(), extent_rect.yMaximum() - ((n_parentgrids_y) * yspacing), extent_rect.xMinimum() + ((n_parentgrids_x) * xspacing), extent_rect.yMaximum())
-        
-        if n_totalcells > 1000000:
-            feedback.pushWarning('Settings will create ' + str(n_parentgrids_t) + ' parent-gridcells and ' + str(n_totalcells-n_parentgrids_t) + ' child-gridcells for ' + str(subgrids-1) + ' childgrids. (= ' + str(n_totalcells) + ' gridcells in total)')
-            feedback.pushWarning('This may take a while!')
-            feedback.pushWarning('Consider choosing a smaller extent, fewer subgrids or a greater spacing')
-        else:
-            feedback.setProgressText('Settings will create ' + str(n_parentgrids_t) + ' parent-gridcells and ' + str(n_totalcells-n_parentgrids_t) + ' child-gridcells for ' + str(subgrids-1) + ' childgrids. (= ' + str(n_totalcells) + ' gridcells in total)')
-                
-        total = 100.0 / n_totalcells if n_totalcells > 0 else 0
-        current = 0
-        
         feedback.setProgressText('Start processing...')
-        start_pointxy = QgsPointXY(extent_rect.xMinimum(),extent_rect.yMaximum())
-        
         if gridtype == 0:
-            step_x = 2**((subgrids-1)*-1)*xspacing
-            step_y = 2**((subgrids-1)*-1)*yspacing
+            if startwithparent:
+                iterationrange = range(1,subgrids+1,1)
+                step_x = xspacing
+                step_y = yspacing
+            else:
+                iterationrange = range(subgrids,0,-1)
+                step_x = xfactor**((subgrids-1)*-1)*xspacing
+                step_y = yfactor**((subgrids-1)*-1)*yspacing
+            
+            n_parentgrids_x = int(math.ceil(extent_rect.width() / xspacing))
+            n_parentgrids_y = int(math.ceil(extent_rect.height() / yspacing))
+            n_parentgrids_t = (n_parentgrids_x * n_parentgrids_y)
+            extent_rect_new = QgsRectangle(extent_rect.xMinimum(), extent_rect.yMaximum() - (n_parentgrids_y * yspacing), extent_rect.xMinimum() + (n_parentgrids_x * xspacing), extent_rect.yMaximum())
+            n_totalcells = 0
+            xyfactor = xfactor * yfactor
+            cells_per_subgrid = {}
+            xcells_per_subgrid = {}
+            ycells_per_subgrid = {}
+            for i in range(1,subgrids+1):
+                #fac = ((2**(i-1))**2)
+                childcells_per_parentcell = int(xyfactor**(i-1))
+                n_totalcells += childcells_per_parentcell * n_parentgrids_t
+                cells_per_subgrid[i] = childcells_per_parentcell * n_parentgrids_t
+                xcells_per_subgrid[i] = int(xfactor**(i-1) * n_parentgrids_x)
+                ycells_per_subgrid[i] = int(yfactor**(i-1) * n_parentgrids_y)
+            if n_totalcells > 1000000:
+                feedback.pushWarning('Settings will create ' + str(n_parentgrids_t) + ' parent-gridcells and ' + str(n_totalcells-n_parentgrids_t) + ' child-gridcells for ' + str(subgrids-1) + ' childgrids. (= ' + str(n_totalcells) + ' gridcells in total)')
+                feedback.pushWarning('This may take a while!')
+                feedback.pushWarning('Consider choosing a smaller extent, fewer subgrids, a greater spacing or a lower x/y factor')
+            else:
+                feedback.setProgressText('Settings will create ' + str(n_parentgrids_t) + ' parent-gridcells and ' + str(n_totalcells-n_parentgrids_t) + ' child-gridcells for ' + str(subgrids-1) + ' childgrids. (= ' + str(n_totalcells) + ' gridcells in total)')
+                
+            total = 100.0 / n_totalcells if n_totalcells > 0 else 0
+            current = 0
+            
             fid = 1
-            for subgrid in range(subgrids, 0, -1):
+            for subgrid in iterationrange:
                 if feedback.isCanceled():
                     break
                 if subgrid == 1:
@@ -167,88 +192,89 @@ class CreateNestedGrid(QgsProcessingAlgorithm):
                 c_y_id = 1
                 parentcellindicator_x = 1
                 parentcellindicator_y = 1
-                current += 1
-                point_topleft_pxy = start_pointxy
-                point_topleft_geom = QgsGeometry.fromPointXY(point_topleft_pxy)
                 
-                while ((point_topleft_pxy.x() <= extent_rect_new.xMaximum()) and (point_topleft_pxy.y() > extent_rect_new.yMinimum())):
+                start_pointxy = QgsPointXY(extent_rect.xMinimum(),extent_rect.yMaximum())
+                point_topleft_pxy = start_pointxy
+                
+                for currenty in range(1,ycells_per_subgrid[subgrid]+1):
                     if feedback.isCanceled():
                         break
+                    # reset x on start of each y row:
+                    point_topleft_pxy = QgsPointXY(start_pointxy.x(),point_topleft_pxy.y())
                     
-                    #point_topright_pxy = point_topleft_pxy.project(step_x,90)
-                    #point_lowerright_pxy = point_topleft_pxy.project(math.sqrt((step_x**2)+(step_y**2)),90+math.degrees(math.atan(step_x/step_y)))
-                    #point_lowerleft_pxy = point_topleft_pxy.project(step_y,180)
-                    point_topright_pxy = point_topleft_pxy.project(step_x,90)
-                    point_lowerright_pxy = point_topright_pxy.project(step_y,180)
-                    point_lowerleft_pxy = point_lowerright_pxy.project(step_x,270)
-                    grid_geom = [point_topleft_pxy,
-                                 point_topright_pxy,
-                                 point_lowerright_pxy,
-                                 point_lowerleft_pxy,
-                                 point_topleft_pxy]
-                    grid_geom = QgsGeometry.fromPolygonXY([grid_geom])
-                    new_feat = QgsFeature(output_layer_fields)
-                    new_feat.setGeometry(grid_geom)
-                    new_feat['x_cent'] = grid_geom.centroid().asPoint().x()
-                    new_feat['y_cent'] = grid_geom.centroid().asPoint().y()
-                    new_feat['v_coords'] = ';'.join([str(vert.x())+','+str(vert.y()) for vert in grid_geom.vertices()])
-                    new_feat['x_space'] = step_x
-                    new_feat['y_space'] = step_y
-                    new_feat['fid'] = fid
-                    uid = str(subgrid).zfill(len(str(subgrids)))
-                    if 0 in letters:
-                        new_feat['p_x_id'] = self.num_to_char(p_x_id)
-                        new_feat['c_x_id'] = self.num_to_char(c_x_id)
-                        uid = uid + '_' + self.num_to_char(p_x_id) + '_' + self.num_to_char(c_x_id)
-                    else:
-                        new_feat['p_x_id'] = p_x_id
-                        new_feat['c_x_id'] = c_x_id
-                        uid = uid + '_' + str(p_x_id) + '_' + str(c_x_id)
-                    if 1 in letters:
-                        new_feat['p_y_id'] = self.num_to_char(p_y_id)
-                        new_feat['c_y_id'] = self.num_to_char(c_y_id)
-                        uid = uid + '_' + self.num_to_char(p_y_id) + '_' + self.num_to_char(c_y_id)
-                    else:
-                        new_feat['p_y_id'] = p_y_id
-                        new_feat['c_y_id'] = c_y_id
-                        uid = uid + '_' + str(p_y_id) + '_' + str(c_y_id)
-                    new_feat['s_id'] = subgrid
-                    new_feat['uid'] = uid
+                    for currentx in range(1,xcells_per_subgrid[subgrid]+1):
+                        if feedback.isCanceled():
+                            break
+                        point_topright_pxy = point_topleft_pxy.project(step_x,90)
+                        point_lowerright_pxy = point_topright_pxy.project(step_y,180)
+                        point_lowerleft_pxy = point_lowerright_pxy.project(step_x,270)
+                        grid_geom = [point_topleft_pxy,
+                                     point_topright_pxy,
+                                     point_lowerright_pxy,
+                                     point_lowerleft_pxy,
+                                     point_topleft_pxy]
+                        grid_geom = QgsGeometry.fromPolygonXY([grid_geom])
+                        new_feat = QgsFeature(output_layer_fields)
+                        new_feat.setGeometry(grid_geom)
+                        new_feat['x_cent'] = grid_geom.centroid().asPoint().x()
+                        new_feat['y_cent'] = grid_geom.centroid().asPoint().y()
+                        new_feat['v_coords'] = ';'.join([str(vert.x())+','+str(vert.y()) for vert in grid_geom.vertices()])
+                        new_feat['x_space'] = step_x
+                        new_feat['y_space'] = step_y
+                        new_feat['fid'] = fid
+                        uid = str(subgrid).zfill(len(str(subgrids)))
+                        if 0 in letters:
+                            new_feat['p_x_id'] = self.num_to_char(p_x_id)
+                            new_feat['c_x_id'] = self.num_to_char(c_x_id)
+                            uid = uid + '_' + self.num_to_char(p_x_id) + '_' + self.num_to_char(c_x_id)
+                        else:
+                            new_feat['p_x_id'] = p_x_id
+                            new_feat['c_x_id'] = c_x_id
+                            uid = uid + '_' + str(p_x_id) + '_' + str(c_x_id)
+                        if 1 in letters:
+                            new_feat['p_y_id'] = self.num_to_char(p_y_id)
+                            new_feat['c_y_id'] = self.num_to_char(c_y_id)
+                            uid = uid + '_' + self.num_to_char(p_y_id) + '_' + self.num_to_char(c_y_id)
+                        else:
+                            new_feat['p_y_id'] = p_y_id
+                            new_feat['c_y_id'] = c_y_id
+                            uid = uid + '_' + str(p_y_id) + '_' + str(c_y_id)
+                        new_feat['s_id'] = subgrid
+                        new_feat['uid'] = uid
                     
-                    sink.addFeature(new_feat, QgsFeatureSink.FastInsert)
-                    fid += 1
+                        sink.addFeature(new_feat, QgsFeatureSink.FastInsert)
+                        fid += 1
                     
-                    current += 1
-                    feedback.setProgress(int(current * total))
+                        current += 1
+                        feedback.setProgress(int(current * total))
                     
-                    #next x:
-                    point_topleft_pxy = QgsPointXY(point_topleft_pxy.x()+step_x,point_topleft_pxy.y())
-                    point_topleft_geom = QgsGeometry.fromPointXY(point_topleft_pxy)
-                    if 2**(subgrid-1) == parentcellindicator_x:
-                        p_x_id += 1
-                        parentcellindicator_x = 0
-                    parentcellindicator_x += 1
-                    c_x_id += 1
+                        #next x:
+                        point_topleft_pxy = QgsPointXY(point_topleft_pxy.x()+step_x,point_topleft_pxy.y())
+                        if xfactor**(subgrid-1) == parentcellindicator_x:
+                            p_x_id += 1
+                            parentcellindicator_x = 0
+                        parentcellindicator_x += 1
+                        c_x_id += 1
                     
                     #next y (reset x):
-                    if point_topleft_pxy.x() >= extent_rect_new.xMaximum() and point_topleft_pxy.y() >= extent_rect_new.yMinimum():
-                        point_topleft_pxy = QgsPointXY(start_pointxy.x(),point_topleft_pxy.y() - step_y)
-                        point_topleft_geom = QgsGeometry.fromPointXY(point_topleft_pxy)
-                        parentcellindicator_x = 1
-                        p_x_id = 1
-                        c_x_id = 1
-                        if 2**(subgrid-1) == parentcellindicator_y:
-                            p_y_id += 1
-                            parentcellindicator_y = 0
-                        parentcellindicator_y += 1
-                        c_y_id += 1
+                    point_topleft_pxy = QgsPointXY(start_pointxy.x(),point_topleft_pxy.y() - step_y)
+                    parentcellindicator_x = 1
+                    p_x_id = 1
+                    c_x_id = 1
+                    if yfactor**(subgrid-1) == parentcellindicator_y:
+                        p_y_id += 1
+                        parentcellindicator_y = 0
+                    parentcellindicator_y += 1
+                    c_y_id += 1
                         
                 #next subgrid:
-                step_x = step_x * 2
-                step_y = step_y * 2
+                if startwithparent:
+                    step_x = step_x / xfactor
+                    step_y = step_y / yfactor
+                else:
+                    step_x = step_x * xfactor
+                    step_y = step_y * yfactor
                 
-                
-        
         """
         elif gridtype == 1:
             grid_geom = [QgsPointXY(parent_x_coord+math.sin(angle)*(xspacing), parent_y_coord+math.cos(angle)*(yspacing)) for angle in self.linspace(0,(2*math.pi),polygonsides,False)]
